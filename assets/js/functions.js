d211ccfbef5f77f06bd69b7e99ffd35d9f661650
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-09-30 16:46:18 +0200

    Modifica
    Modifica Functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,7 +15,7 @@ function menu(){
 
       for(var i = 0; commits.number_chapter >= i; i++){
         myTable.insertAdjacentHTML("beforeend",
-        `<tr id="prova"><td>${commits.chapters[i]}</td><td>Prova</td><td>Prova</td></tr>`)
+        `<tr id="prova"><td>Prova</td><td>Prova</td><td>Prova</td></tr>`)
       }
 
     })
