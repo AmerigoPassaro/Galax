58e8baec62ed130fcae6c9bc0bf9604bd9248748
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-09-30 15:40:31 +0200

    Modifica
    Modifica Index, Functions e Riassunti

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,22 @@
 let mySection = document.querySelector("#main-section");
+let myTable = document.querySelector("#main-table-body");
 
 function menu(){
 
-  mySection.innerHTML = "Sono vuoto!" ;
+  mySection.innerHTML = "" ;
 
   let menuOption = event.target.id
 
   fetch(`https://amerigopassaro.github.io/Galax/assets/js/riassunti/${menuOption}.json`)
     .then(response => response.json())
-    .then(commits => alert(commits))
+    .then(commits => {
+      for (let i = 0; i < commits.length; i++){
+        bodyTable.insertAdjacentHTML("beforeend",
+        `<tr id="${i}">
+        <td>${i+1}</td>
+        <td>${comits[i]}</td>
+        <td>${i+1}</td>
+        </tr>`)}
+    })
 
 }
