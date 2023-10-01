926318965da265f9858dcd55e461e89340686fc1
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-10-01 17:50:31 +0200

    Prova
    Modifica di prova Functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,15 +7,14 @@ function menu(){
 
   let menuOption = event.target.id
 
+  myTable.insertAdjacentHTML("beforeend",
+  `<tr id="prova"><td>Prova</td><td>Prova</td><td>Prova</td></tr>`)
 
 
   fetch(`https://amerigopassaro.github.io/Galax/assets/js/riassunti/${menuOption}.json`)
     .then(response => response.json())
     .then(commits => {
 
-      for(var i = 0; commits.number_chapter >= i; i++){
-        myTable.insertAdjacentHTML("beforeend",
-        `<tr id="prova"><td>Prova</td><td>Prova</td><td>Prova</td></tr>`)
       }
 
     })
