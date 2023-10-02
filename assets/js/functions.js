4983ab21df7c24de2893ced9379c20b8b7a2d399
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-10-02 22:36:17 +0200

    Modifica
    Modifica Functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,23 +1,25 @@
 let mySection = document.querySelector("#main-section");
 let myTable = document.querySelector("#main-table-body");
 
+function openChapterList(){
+  fetch(`https://amerigopassaro.github.io/Galax/assets/js/riassunti/${menuOption}.json`)
+    .then(response => response.json())
+    .then(commits => {
+      for(var i = 0; commits.number_chapter >= i; i++){
+        myTable.insertAdjacentHTML("beforeend",
+        `<tr id="prova"><td>${i+1}</td><td>${commits.chapters[i].name}</td><td>${commits.chapters[i].num_episodes}</td></tr>`)
+      }
+    }
+  )
+}
+
+
 function menu(){
 
   mySection.innerHTML = "" ;
 
   let menuOption = event.target.id
 
-
-
-  fetch(`https://amerigopassaro.github.io/Galax/assets/js/riassunti/${menuOption}.json`)
-    .then(response => response.json())
-    .then(commits => {
-
-      for(var i = 0; commits.number_chapter >= i; i++){
-        myTable.insertAdjacentHTML("beforeend",
-        `<tr id="prova"><td>${i+1}</td><td>${commits.chapters[i].name}</td><td>${commits.chapters[i].num_episodes}</td></tr>`)
-      }
-
-    })
+  openChapterList()
 
 }
