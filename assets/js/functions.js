2fa466e2c783af8342b6e284166a7290dff81f20
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-10-02 22:41:18 +0200

    Modifica
    Modifica Functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,25 +1,26 @@
 let mySection = document.querySelector("#main-section");
 let myTable = document.querySelector("#main-table-body");
 
-function openChapterList(){
+function menu(){
+
+  // Cancellazione elementi nella section
+  mySection.innerHTML = "" ;
+  // Individuazione elemento menu cliccato
+  let menuOption = event.target.id
+  // Collegamento al Json
   fetch(`https://amerigopassaro.github.io/Galax/assets/js/riassunti/${menuOption}.json`)
     .then(response => response.json())
+  // Estrapolazione e visualizzazione dei "record"
     .then(commits => {
       for(var i = 0; commits.number_chapter >= i; i++){
         myTable.insertAdjacentHTML("beforeend",
-        `<tr id="prova"><td>${i+1}</td><td>${commits.chapters[i].name}</td><td>${commits.chapters[i].num_episodes}</td></tr>`)
+        `<tr id="prova">
+          <td>${i+1}</td>
+          <td>${commits.chapters[i].name}</td>
+          <td>${commits.chapters[i].num_episodes}</td>
+        </tr>`)
       }
-    }
-  )
-}
+    })
 
-
-function menu(){
-
-  mySection.innerHTML = "" ;
-
-  let menuOption = event.target.id
-
-  openChapterList()
-
+    
 }
